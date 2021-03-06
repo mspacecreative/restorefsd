f3ac15b89f968c28adb8043f2805fdd5e596aa67
Matt <matt@mspacecreative.com>
2021-03-06 11:17:54 -0400

    404 page

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+get_header(); ?>
+
+<section class="section">
+	<div class="inner_container narrow">
+		
+		<h1><?php esc_html_e('Page not found'); ?></h1>
+	
+		<?php esc_html_e('Sorry, this page does not exist. Click '); ?><a href="<?php echo home_url(); ?>here</a><?php esc_html_e(' to go to the home page'); ?></p>
+		
+	</div>
+</section>
+
+<?php get_footer();
