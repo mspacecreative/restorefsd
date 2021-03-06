71ca1f61386b243697daad988c9c6f95c7ebb0eb
Matt <matt@mspacecreative.com>
2021-03-06 11:22:57 -0400

    c

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@ get_header(); ?>
 		
 		<h1><?php esc_html_e('Page not found'); ?></h1>
 	
-		<?php esc_html_e('Sorry, this page does not exist. Click '); ?><a href="<?php echo home_url(); ?>">here</a><?php esc_html_e(' to go to the home page'); ?></p>
+		<p><?php esc_html_e('Sorry, this page does not exist. Click '); ?><a href="<?php echo home_url(); ?>">here</a><?php esc_html_e(' to go to the home page'); ?></p>
 		
 	</div>
 </section>
