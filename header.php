3958efb3ea734dd0ca5c005feea48902b54cad13
Matt <matt@mspacecreative.com>
2021-03-08 15:30:12 -0400

    Branding added to header

--- header.php
+++ header.php
@@ -23,3 +23,11 @@
 		<!--[if lt IE 7]>
 		<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 		<![endif]-->
+	
+		<header>
+			<div class="inner_container narrow">
+				<div class="logo">
+					<?php include 'assets/img/branding/restoreFSD-logo.php'; ?>
+				</div>
+			</div>
+		</header>
